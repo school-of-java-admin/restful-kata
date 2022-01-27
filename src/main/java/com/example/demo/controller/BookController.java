606e01c69e48a9463f0a6805ef0865c6e904c5fc
Todor Radev <Todor.Radev@endava.com>
2022-01-27 15:50:00 +0200

    test

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,66 +1,50 @@
 package com.example.demo.controller;
 
-import com.example.demo.controller.dto.BookDto;
+import com.example.demo.dto.BookDto;
+import com.example.demo.exception.BookNotFoundException;
 import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
-import java.util.List;
-import java.util.Optional;
-import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController         // very important for injection
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
 @RequestMapping("/books")
 public class BookController {
 
-    private final BookService bookService;
+    @Autowired
+    private BookService bookService;
 
     private ModelMapper modelMapper = new ModelMapper();
 
-    public BookController(BookService bookService) {
-      this.bookService = bookService;
-    }
-
     @PostMapping
     ResponseEntity<BookDto> createBook(@Valid @RequestBody BookDto newBook) {
-      Book book = modelMapper.map(newBook, Book.class);
-      BookDto mappedDto = modelMapper.map(bookService.saveBook(book), BookDto.class);
-      return ResponseEntity.ok(mappedDto);
+        Book book = modelMapper.map(newBook, Book.class);
+        Book createdBook = bookService.saveBook(book);
+        BookDto mappedDto = modelMapper.map(createdBook, BookDto.class);
+        return ResponseEntity.ok(mappedDto);
     }
 
-    ResponseEntity<BookDto> findById(Long id) {
-      Optional<Book> optionalBook = bookService.findById(id);
-      if(!optionalBook.isPresent()) {
-      
-      }
-
-      return null;
+    @GetMapping("/{id}")
+    ResponseEntity<BookDto> findById(@PathVariable(value = "id") Long id) {
+        Optional<Book> optionalBook = bookService.findById(id);
+        if (optionalBook.isEmpty()) {
+            //return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            throw new BookNotFoundException(id);
+        }
+        BookDto mappedDto = modelMapper.map(optionalBook.get(), BookDto.class);
+        return ResponseEntity.ok(mappedDto);
     }
 
-    @GetMapping("/page/{pageNo}")
-    public Page<BookDto> booksPaginated(@PathVariable (value = "pageNo") Integer pageNo) {
-      Integer pageSize = 5;
-      Page<Book> booksPage = bookService.findPaginated(pageNo, pageSize);
-
-      System.out.println("Current Page" + pageNo);
-      System.out.println("======================== TOTAL PAGES ======================");
-      System.out.println(booksPage.getTotalPages());
-      System.out.println("======================== TOTAL ELEMENTS ======================");
-      System.out.println(booksPage.getTotalElements());
-      System.out.println("======================== TOTAL PAGE CONTENT ======================");
-      System.out.println(booksPage.getContent());
-
-
-
-      return null;
+    @GetMapping
+    public Page<Book> getBooks(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
-
-
 }
